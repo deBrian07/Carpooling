f2a8b163340485a00bd1a3e4dea240dfb8c6a8a7
Brian Chen <debriann07@gmail.com>
2024-07-11 22:30:26 -0700

    Initial Commit

---
+++ Carpooling/CarpoolingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarpoolingApp.swift
+//  Carpooling
+//
+//  Created by Brian Chen on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CarpoolingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
